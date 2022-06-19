6e9c89d6afa2b8150c7840666fd00f9d4db3379d
yewon0804 <ehdnjs0804@gmail.com>
2022-06-19 23:52:46 +0900

    Recursion - Fibonacci number (#15)
    Recursion을 이용하되,
    한 번 계산한 함수 값은 반복해서 계산하지 않도록

---
+++ data_structure/fiboMemo.java
@@ -0,0 +1,30 @@
+package data_structure;
+
+import java.util.Scanner;
+
+public class fiboMemo {
+    public static final int howMuch = 100;
+    public static long fibArray[] = new long[howMuch];
+    
+    public static long fibonacci(long n){
+        long fibValue = 0;
+
+        if(n == 0) return 0;            // base case
+        else if(n == 1) return 1;       // base case
+        else{
+            fibValue=fibonacci(n - 1) + fibonacci(n - 2);
+            fibArray[(int)n] = fibValue;
+            return fibValue;
+        }
+    }
+    
+    public static void main(String args[]){
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+
+        System.out.println("Enter n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("F(" + n + ") = " + fibonacci(n));
+    }
+}
