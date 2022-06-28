578cd16ff2dda109ca922a440a3c402263a9f74e
yewon0804 <ehdnjs0804@gmail.com>
2022-06-28 15:51:24 +0900

    Resolve boj2525.java (#1)
    빠르게 입력 받기! Scanner는 느리다..
    입력을 위한 클래스는 하나만 쓸 것!
    출력도 System.out.println()은 느리다..
    BufferReader(+ StringTokenizer), BufferWriter로 바꿔보았습니다!
    + StringBuilder로 출력할 것들을 모두 모아준 다음,
      그냥 System.out.println()으로 출력해주는 방식도 있다..

--- boj/boj2525.java
+++ boj/boj2525.java
@@ -1,14 +1,23 @@
 package boj;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class boj2525 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 앞으로 br, bw는 세트로 생각하겠어요
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int m = sc.nextInt();
-        int s = sc.nextInt();
-        int howMuch = sc.nextInt();
+        int m = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        // 변수 한 개는 StringTokenizer가 아니라!! 기본 입력을 사용해요
+        int howMuch = Integer.parseInt(br.readLine());
 
         s += howMuch;
 
@@ -20,6 +29,11 @@ public class boj2525 {
             m -= 24;
         }
 
-        System.out.println(m + " " + s);
+        // System.out.println(m + " " + s); -> 느립니다.. bw로 대체 할거예요
+        // bw.write(String.valueOf(m + " " + s)); -> String에 대한 '+' 연산 자체가 느리다.. 빠른 출력을 쓰는 의미 퇴색
+        bw.write(String.valueOf(m));
+        bw.write(String.valueOf(" "));
+        bw.write(String.valueOf(s));
+        bw.flush();
     }
 }
