803987bf55ea957c4c0ade3210b3be0f5e00df6a
yewon0804 <ehdnjs0804@gmail.com>
2022-07-03 01:23:11 +0900

    boj 2675 - never used 요소 삭제

--- boj/boj2675.java
+++ boj/boj2675.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 public class boj2675 {
