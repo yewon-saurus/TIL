d02b07dea6388f742728d11907c8099842dcb967
yewon0804 <ehdnjs0804@gmail.com>
2022-07-17 23:37:33 +0900

    boj 1316 - 그룹 단어 체커 (#17)

---
+++ boj/boj1316.java
@@ -0,0 +1,44 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj1316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+			
+            String S = st.nextToken();
+			boolean check[] = new boolean[26];
+			boolean tmp = true;
+			
+			for (int j = 0; j < S.length(); j++) {
+				int index = S.charAt(j) - 'a';
+				if (check[index]) {
+					if(S.charAt(j) != S.charAt(j - 1)) {
+						tmp = false;
+						break;
+					}
+				}
+                else {
+					check[index] = true;
+				}
+			}
+			if (tmp) cnt++;
+		}
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+}
