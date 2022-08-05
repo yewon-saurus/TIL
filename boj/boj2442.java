18ed81abbb7490ae18a71b7b4270bf6efa36eaae
yewon0804 <ehdnjs0804@gmail.com>
2022-08-05 16:27:09 +0900

    boj 2442 - 별 찍기 5 - 출력에 BufferedWriter 이용

--- boj/boj2442.java
+++ boj/boj2442.java
@@ -1,23 +1,28 @@
 package boj;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class boj2442 {
     public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                bw.write(" ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
+
+        bw.flush();
     }
 }
