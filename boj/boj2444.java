1f5d2ec339c7c42ee8778ab01fb0cfd50f603445
yewon0804 <ehdnjs0804@gmail.com>
2022-08-06 19:20:58 +0900

    boj 2444번 : 별 찍기 - 7

---
+++ boj/boj2444.java
@@ -0,0 +1,37 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj2444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            if (i <= n) {
+                for (int j = 1; j <= n - i; j++) {
+                    bw.write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    bw.write("*");
+                }
+            }
+            else {
+                for (int j = 1; j <= i % n; j++) {
+                    bw.write(" ");
+                }
+                for (int j = 1; j <= (2 * n - 1) - 2 * (i % n); j++) {
+                    bw.write("*");
+                }
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
