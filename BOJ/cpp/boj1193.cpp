39b791663c5d4db2a20fc76562cacde8734e533e
yewon0804 <ehdnjs0804@gmail.com>
2022-12-24 19:58:34 +0900

    boj: 1193.cpp

---
+++ BOJ/cpp/boj1193.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (n > i) {
+        n -= i;
+        i++;
+    }
+
+    if (i % 2 == 1) {
+        cout << i + 1 - n << '/' << n << "\n";
+    }
+    else {
+        cout << n << '/' << i + 1 - n << "\n";
+    }
+
+    return 0;
+}
