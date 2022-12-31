a0790fc2d22b2293f83e3b6104bd228dbfd62d13
yewon0804 <ehdnjs0804@gmail.com>
2022-12-31 17:44:04 +0900

    BOJ: 2164 카드2 - S4

---
+++ BOJ/cpp/2164.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    
+    cout << q.front() << "\n";
+    return 0;
+}
