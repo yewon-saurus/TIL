c27cea8a41ee4b0201a4916081d2972bf261ad64
yewon0804 <ehdnjs0804@gmail.com>
2023-03-08 00:19:39 +0900

    BOJ: 14478번: 서로 다른 부분 문자열의 개수
    - set

--- CodingTest/main.cpp
+++ CodingTest/main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <vector>
+#include <set>
 using namespace std;
 
+// 서로 다른 부분 문자열의 개수
+// 11478 - S3
+// https://www.acmicpc.net/problem/11478
+
 int main() {
-    cout << "Hello, world!" << endl;
-    vector<int> v = {1, 2, 3};
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
 
-    /*
-        아놔.. ㅋㅋ
-        vscode 터미널에서
-        vector같은 STL이 포함된 cpp 파일을 컴파일 해 산출된 실행파일이
-        실행은 되는데 오류 로그도 없고 실행 결과도 출력되지 않아서
-        한참을 삽질 했는데..
-        Windows Terminal 에서 똑같은 파일 실행해보니
-        정상적으로 출력 되네요 ^^
-    */
+    string S;
+    cin >> S;
 
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i];
-        cout << " hi" << i << endl;
+    set<string> set;
+
+    string tmp;
+    for (int i = 0; i < S.length(); i++) {
+        tmp = "";
+        for (int j = i; j < S.length(); j++) {
+            tmp += S[j];
+            set.insert(tmp);
+        }
     }
 
+    cout << set.size();
     return 0;
 }
